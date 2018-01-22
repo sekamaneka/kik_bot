b93a31fee2458f7bce7a12a3c34173630f4a295b
stef <swilso793@gmail.com>
2018-01-22 18:21:02 +0100

    Try out.

--- zefeelz/agoogol.py
+++ zefeelz/agoogol.py
@@ -51,28 +51,27 @@ def response_picker(message):
     print(sentiment)
     print(type(sentiment))
     accuracy = max(sentiment.p_pos, sentiment.p_neg)
-    if polarity > 0:
-        txt_polar = "Your tone is {}% Positive\n".format(polarity * 100)
-    if polarity < 0:
-        txt_polar = "Your tone is {}% Negative\n".format(polarity * 100)
-    if polarity == 0:
-        txt_polar = "Your tone is Neutral. Like a Machine.\n"
-    if subjectivity > 0.80:
-        txt_subj = "Your truth is very subjective.\n"
-    elif subjectivity > 0.6:
-        txt_subj = "Your truth is pretty subjective.\n"
-    if subjectivity < 0.4:
-        txt_subj = "Your truth is pretty objective.\n"
-    if subjectivity < 0.2:
-        txt_subj = "Your truth is very objective.\n"
-
-    if sentiment_label == 'pos':
-        sentiment_label = 'Positive Vibes!'
-    if sentiment_label == 'neg':
-        sentiment_label = 'Negative Vibes :('
+    # if polarity > 0:
+    # txt_polar="Your tone is {}% Positive\n".format(polarity * 100)
+    # if polarity < 0:
+    # txt_polar="Your tone is {}% Negative\n".format(polarity * 100)
+    # if polarity == 0:
+    # txt_polar="Your tone is Neutral. Like a Machine.\n"
+    # if subjectivity > 0.80:
+    # txt_subj="Your truth is very subjective.\n"
+    # elif subjectivity > 0.6:
+    # txt_subj="Your truth is pretty subjective.\n"
+    # if subjectivity < 0.4:
+    # txt_subj="Your truth is pretty objective.\n"
+    # if subjectivity < 0.2:
+    # txt_subj="Your truth is very objective.\n"
+    # if sentiment_label == 'pos':
+    #sentiment_label = 'Positive Vibes!'
+    # if sentiment_label == 'neg':
+    #sentiment_label = 'Negative Vibes :('
     # send_messages(message, text_to_send=txt_polar + txt_subj)
-    send_messages(message, text_to_send="Sentiment: {}\nSubjectivity: {}%\nAccuracy: {}%\nPolarity: {}%".format(
-        sentiment_label, subjectivity * 100, round(accuracy, 2), round(polarity * 100, 0)))
+    send_messages(message, text_to_send="Subjectivity: {}%\nAccuracy: {}%\nPolarity: {}%".format(
+        subjectivity * 100, round(accuracy, 2), round(polarity * 100, 0)))
 
 
 def send_messages(message, inc_url="", text_to_send="", instant_pic="", link=0, inc_title=""):
--- zefeelz/train.py
+++ zefeelz/train.py
@@ -5,30 +5,30 @@ from textblob.classifiers import NaiveBayesClassifier
 from textblob.sentiments import NaiveBayesAnalyzer
 
 
-# def parser(row):
-#sent = row[1].encode('utf-8', 'ignore').decode('utf-8')
-# if sent == '0':
-#sentiment = 'neg'
-# else:
-#sentiment = 'pos'
-# return row[-1].strip(), sentiment
-#
-#
-# def data():
-#training = []
-# with open('Sentiment Analysis Dataset.csv', 'r') as csvf:
-#reader = csv.reader(csvf)
-#reader = list(reader)
-# for row in range(0, len(reader), 1000):
-#sentence_tuple = parser(reader[row])
-# training.append(sentence_tuple)
-# return training
+def parser(row):
+    sent = row[1].encode('utf-8', 'ignore').decode('utf-8')
+    if sent == '0':
+        sentiment = 'neg'
+    else:
+        sentiment = 'pos'
+    return row[-1].strip(), sentiment
 
 
-# def train():
-#training = data()
-#classifier = NaiveBayesClassifier(training)
-# return classifier
+def data():
+    training = []
+    with open('Sentiment Analysis Dataset.csv', 'r') as csvf:
+        reader = csv.reader(csvf)
+        reader = list(reader)
+        for row in range(0, len(reader), 1000):
+            sentence_tuple = parser(reader[row])
+            training.append(sentence_tuple)
+    return training
+
+
+def train():
+    training = data()
+    classifier = NaiveBayesClassifier(training)
+    return classifier
 
 
 # try:
@@ -43,14 +43,17 @@ from textblob.sentiments import NaiveBayesAnalyzer
 
 
 print('Data loaded.')
-blobber = Blobber(analyzer=NaiveBayesAnalyzer())  # , classifier = cl)
+blobber = Blobber(analyzer=NaiveBayesAnalyzer(), classifier=train())  # , classifier = cl)
 print('Data trained')
 send = blobber('And i once knew you are a dick.')
 print(send.sentiment)
+print(send.classify())
 send = blobber('You are the worst shit in the world.')
 print(send.sentiment)
+print(send.classify())
 send = blobber('I will always love you you are the best in the world.')
 print(send.sentiment)
+print(send.classify())
 send = blobber('The clasifier is roten.')
 print(send.correct())
 # print(send.classify())
