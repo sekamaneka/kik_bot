43ae042701860ebe5ae3fa122eaa6a6d4d61bbd2
stef <swilso793@gmail.com>
2018-01-22 18:04:19 +0100

    Fixed sutpid mistake.

--- zefeelz/agoogol.py
+++ zefeelz/agoogol.py
@@ -66,9 +66,9 @@ def response_picker(message):
     if subjectivity < 0.2:
         txt_subj = "Your truth is very objective.\n"
 
-    if sentiment_label = 'pos':
+    if sentiment_label == 'pos':
         sentiment_label = 'Positive Vibes.'
-    if sentiment_label = 'neg':
+    if sentiment_label == 'neg':
         sentiment_label = 'Negative Vibes.'
     # send_messages(message, text_to_send=txt_polar + txt_subj)
     send_messages(message, text_to_send="*Polarity: {}%\n*Subjectivity: {}%\n*Sentiment: {}\n*Accuracy: {}%".format(
